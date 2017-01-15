16e77c2a29a2328d117cd309e172cfe59138dc55
octakon <roberto.sortino@mailbox.org>
2017-01-15 11:16:22 +0100

    disabled open tabs button when no sites are stored

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,58 +1,69 @@
 'use strict';
 
 requirejs.config({
-  baseUrl: 'scripts'
+    baseUrl: 'scripts'
 });
 
-requirejs(['utils/tabs', 'utils/storage'], function(tabs, storage) {
-  storage.initSites();
+requirejs(['utils/tabs', 'utils/storage'], function (tabs, storage) {
+    storage.initSites();
 
-  var btnSaveCurrentTab = $('#save-current-tab');
-  var btnSaveAllTabs = $('#save-all-tabs');
-  var btnOpenTabs = $('#open-tabs');
-  var selectPresets = $('#saving-presets');
+    var btnSaveCurrentTab = $('#save-current-tab');
+    var btnSaveAllTabs = $('#save-all-tabs');
+    var btnOpenTabs = $('#open-tabs');
+    var selectPresets = $('#saving-presets');
 
-  var getSelectedPreset = function () {
-    return selectPresets.find('option:selected').val();
-  };
+    var getSelectedPreset = function () {
+        return selectPresets.find('option:selected').val();
+    };
 
-  var saveCurrentTab = function() {
-    tabs.getCurrentTab(function(tab) {
-      storage.setUrlTo(tab.url, getSelectedPreset());
-    });
-  };
+    var saveCurrentTab = function () {
+        tabs.getCurrentTab(function (tab) {
+            storage.setUrlTo(tab.url, getSelectedPreset());
+            triggerButtonToOpenTabs();
+        });
+    };
 
-  var saveAllTabs = function() {
-    tabs.getAllTabs(function(tabs){
-      console.log(tabs);
-      var i = 0;
-      var tabsLength = tabs.length;
-      for (i; i < tabsLength; i++) {
-        var tab = tabs[i];
-        storage.setUrlTo(tab.url, getSelectedPreset());
-      }
-    });
-  };
+    var saveAllTabs = function () {
+        tabs.getAllTabs(function (tabs) {
+            var i = 0;
+            var tabsLength = tabs.length;
+            for (i; i < tabsLength; i++) {
+                var tab = tabs[i];
+                storage.setUrlTo(tab.url, getSelectedPreset());
+            }
+            triggerButtonToOpenTabs();
+        });
+    };
 
-  var openTabs = function() {
-    var sites = storage.getSites();
-    var todaySites = storage.getTodaySites();
-    var i = 0;
-    var numberURLs = sites.everyDayTabs.length;
-    for(i; i < numberURLs; i++) {
-      tabs.createTab(sites.everyDayTabs[i]);
-    }
-    if(!_.isNull(todaySites)) {
-      i=0;
-      numberURLs = todaySites.length;
-      for(i; i < numberURLs; i++) {
-        tabs.createTab(todaySites[i]);
-      }
-    }
-  };
+    var openTabs = function () {
+        var sites = storage.getSites();
+        var todaySites = storage.getTodaySites();
+        var i = 0;
+        var numberURLs = sites.everyDayTabs.length;
+        for (i; i < numberURLs; i++) {
+            tabs.createTab(sites.everyDayTabs[i]);
+        }
+        if (!_.isNull(todaySites)) {
+            i = 0;
+            numberURLs = todaySites.length;
+            for (i; i < numberURLs; i++) {
+                tabs.createTab(todaySites[i]);
+            }
+        }
+    };
 
-  btnSaveCurrentTab.on('click.sct', saveCurrentTab);
-  btnSaveAllTabs.on('click.sat', saveAllTabs);
-  btnOpenTabs.on('click.gs', openTabs);
+    let triggerButtonToOpenTabs = function () {
+        if (storage.hasTabsToOpen()) {
+            btnOpenTabs.removeClass('disabled');
+            btnOpenTabs.on('click.gs', openTabs);
+        } else {
+            btnOpenTabs.addClass('disabled');
+        }
+    };
+
+    btnSaveCurrentTab.on('click.sct', saveCurrentTab);
+    btnSaveAllTabs.on('click.sat', saveAllTabs);
+
+    triggerButtonToOpenTabs();
 
 });
